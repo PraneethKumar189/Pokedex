b699da2f2bdaf65334740d80b3da9b13b71ca17b
Praneeth Kumar <124180295+PraneethKumar189@users.noreply.github.com>
2025-02-01 19:09:23 +0530

    updated ui

--- src/app/components/view/view.component.ts
+++ src/app/components/view/view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, DoCheck } from '@angular/core';
 import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ApiserviceService } from '../../services/apiservice.service';
@@ -8,89 +8,108 @@ import { SharedService } from '../../services/shared.service';
 @Component({
   selector: 'app-view',
   standalone: true,
-  imports: [NgIf,TitleCasePipe,NgFor,RouterLink,RouterOutlet],
+  imports: [NgIf, TitleCasePipe, NgFor, RouterLink, RouterOutlet],
   templateUrl: './view.component.html',
-  styleUrl: './view.component.css'
+  styleUrls: ['./view.component.css'],
 })
-export class ViewComponent implements OnInit,OnDestroy{
+export class ViewComponent implements OnInit, OnDestroy, DoCheck {
+  pokemon: any = null;
+  subscriptions: Subscription[] = [];
+  desc: string = '';
 
+  constructor(
+    private route: ActivatedRoute,
+    private pokemonservice: ApiserviceService,
+    private shared: SharedService
+  ) {}
 
-pokemon:any=null;
-subscriptions:Subscription[]=[];
-
-constructor(private route:ActivatedRoute,private pokemonservice:ApiserviceService,private shared:SharedService){}
-
-set subscription(subscription:Subscription){
-  this.subscriptions.push(subscription);
-}
+  private addSubscription(subscription: Subscription) {
+    this.subscriptions.push(subscription);
+  }
 
   ngOnInit(): void {
-    this.subscription=this.route.params.subscribe(params=>{
-      if(this.pokemonservice.pokemons.length){
-        this.pokemon=this.pokemonservice.pokemons.find(i=>i.name === params['name']);
-        if(this.pokemon){
-          this.getEvolution();
-          return;
-
-          
+    this.addSubscription(
+      this.route.params.subscribe((params) => {
+        if (this.pokemonservice.pokemons.length) {
+          this.pokemon = this.pokemonservice.pokemons.find(
+            (i) => i.name === params['name']
+          );
+          if (this.pokemon) {
+            this.getEvolution();
+            return;
+          }
         }
-      }
-       console.log(params['name'])
-      this.subscription=this.pokemonservice.get(params['name']).subscribe(response=>{
-        this.pokemon=response;
-        this.getEvolution();
-
-      },error=>console.log('Error Occured:',error))
-    })
-    this.getDescription()
-  }
-  ngOnDestroy():void{
-         this.subscriptions.forEach(subscription=>subscription ? subscription.unsubscribe():0)
-  }
-  getEvolution():any {
-    if (!this.pokemon.evolutions || !this.pokemon.evolutions.length){
-      this.pokemon.evolutions=[]
-      this.subscription=this.pokemonservice.getSpecies(this.pokemon.name).subscribe(response=>{
-        const id:any =this.getId(response.evolution_chain.url);
-    
-       // this.subscriptions.push()
-       console.log(this.subscriptions)
-        this.subscription=this.pokemonservice.getEvalution(id).subscribe(response=> this.getEvolves(response.chain));
-        console.log(response)
+        console.log(params['name']);
+        this.addSubscription(
+          this.pokemonservice.get(params['name']).subscribe(
+            (response) => {
+              this.pokemon = response;
+              this.getEvolution();
+            },
+            (error) => console.log('Error Occurred:', error)
+          )
+        );
       })
+    );
+    this.getDescription();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription?.unsubscribe());
+  }
+
+  getEvolution(): void {
+    if (!this.pokemon.evolutions || !this.pokemon.evolutions.length) {
+      this.pokemon.evolutions = [];
+      this.addSubscription(
+        this.pokemonservice.getSpecies(this.pokemon.name).subscribe((response) => {
+          const id: any = this.getId(response.evolution_chain.url);
+          console.log(this.subscriptions);
+          this.addSubscription(
+            this.pokemonservice.getEvalution(id).subscribe((response) =>
+              this.getEvolves(response.chain)
+            )
+          );
+          console.log(response);
+        })
+      );
     }
   }
+
   getEvolves(chain: any): void {
     this.pokemon.evolutions.push({
-      id:this.getId(chain.species.url),
-      name:chain.species.name
+      id: this.getId(chain.species.url),
+      name: chain.species.name,
     });
-    if(chain.evolves_to.length){
+    if (chain.evolves_to.length) {
       this.getEvolves(chain.evolves_to[0]);
     }
   }
-  getId(url: string) :number{
-    const spliturl=url.split('/');
-    return +spliturl[spliturl.length-2]
+
+  getId(url: string): number {
+    const spliturl = url.split('/');
+    return +spliturl[spliturl.length - 2];
   }
- desc:string='';
- setId(id:any){
-  this.shared.updateData(id);
-  console.log(id)
-}
-ngAfterViewChecked(){
-  this.getDescription()
-}
-  getDescription(){
-   
-    
-    return this.pokemonservice.getSpeciesbyid().subscribe(data=>{
-      
-      this.desc=data.flavor_text_entries[0].flavor_text;
-      console.log(this.desc)
-    
-    })
-      
+
+  setId(id: any): void {
+    this.shared.updateData(id);
+    console.log(id);
   }
-  
+
+  ngDoCheck(): void {
+    this.getDescription();
+  }
+
+  getDescription(): void {
+    if (this.pokemon) {
+      this.addSubscription(
+        this.pokemonservice.getSpeciesbyid().subscribe((data) => {
+          if (data?.flavor_text_entries?.length) {
+            this.desc = data.flavor_text_entries[0].flavor_text;
+            console.log(this.desc);
+          }
+        })
+      );
+    }
+  }
 }
--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -2,19 +2,16 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SharedService {
-
-
-
-  private dataSource = new BehaviorSubject<string>(''); 
-  currentData = this.dataSource.asObservable(); 
+  private dataSource = new BehaviorSubject<string>(''); // Initial value
+  currentData = this.dataSource.asObservable(); // Expose as Observable
 
   constructor() {}
 
   updateData(newData: string) {
-    this.dataSource.next(newData); 
-    console.log(this.currentData)
+    this.dataSource.next(newData);
+    console.log('Updated Data:', this.dataSource.getValue()); // ✅ Logs actual value
   }
 }
